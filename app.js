bf674cdabbc46c9c4065c600821c947a5e719bb5
dikshitakambri <dikshitakambri18@gmail.com>
2021-01-02 15:16:25 +0530

    second commit

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ app.post("/", function(req, res) {
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const weatherdescription = weatherData.weather[0].description
+            const pressure = weatherData.main.pressure
+            const humidity = weatherData.main.humidity
+            const wind = weatherData.wind.speed
             const icon = weatherData.weather[0].icon
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<h1>Temperature of " + City +" is " + temp + " degree celcius</h1>");
             res.write("<p>Weather of " + City+" is " + weatherdescription + "</p>");
+            res.write("<p>Pressure:" + pressure + "</>");
+            res.write("<p>Humidity:" + humidity + "</>");
+            res.write("<p>Wind speed is " + wind + "</>");
             res.write("<img src=" + imageURL +">");
             res.send();
         })
