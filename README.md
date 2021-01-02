5978cf1b923649ea83908aa9b12417200eaa00c7
dikshitakambri <dikshitakambri18@gmail.com>
2021-01-02 14:54:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Weather Forecast🌨️</h1>
+
+<p>In this web application, you need to enter your city name and it will give you related weather details.⛈️</p>
+
+<h4>Languages Used</h4>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript - Node.js</li>
+</ul>
+
+<h4>Framework Used</h4>
+<ul>
+    <li>Bootstrap(Frontend)</li>
+    <li>Express.js(Backend)</li>
+</ul>
+
+<h4>API Used</h4>
+<li><a href="https://openweathermap.org">Openweathermap</a></li>
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+    var City = (req.body.city);
+    var apiKey = "9216e05e696f5cff415d1e822382ca06";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + City +"&appid=" + apiKey +"&units=metric";
+    https.get(url, function(response ) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherdescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<h1>Temperature of " + City +" is " + temp + " degree celcius</h1>");
+            res.write("<p>Weather of " + City+" is " + weatherdescription + "</p>");
+            res.write("<img src=" + imageURL +">");
+            res.send();
+        })
+    });
+});
+
+    
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
